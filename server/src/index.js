c809d26797c8073daaa5836e7d9ad730adac0d05
DevaanshKathuria <devaanshkathuria@gmail.com>
2026-02-08 02:03:31 +0530

    Updated server/src/index.js

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
+const mongoose = require('mongoose');
 const app = require('./app');
 
 const PORT = process.env.PORT || 5001;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+mongoose.connect(MONGO_URI)
+  .then(() => {
+    console.log('MongoDB Connected');
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
